c96751f9b4d46901726337f4886ff6d7cc40efca
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-08-31 12:41:05 -0300

    ✨ List files in a directory

---
+++ srs/filesystem.py
@@ -0,0 +1,13 @@
+from os import listdir
+from os.path import isfile, join
+
+
+"""
+  Lista todos os arquivos de um dado diretorio
+
+  Retorna um lista de strings com os nomes dos arqivos
+"""
+def listDirFiles(dirPath: str) -> [str]: 
+  onlyfiles = [f for f in listdir(dirPath) if isfile(join(dirPath, f))]
+  return onlyfiles;
+
