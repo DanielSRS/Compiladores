6ae9ff7222e3cea3b2dbb33c8c5ded60fe75e6b7
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-08-31 13:02:53 -0300

    🏷️ Fix type of listDirFiles

--- srs/filesystem.py
+++ srs/filesystem.py
@@ -7,7 +7,7 @@ from os.path import isfile, join
 
   Retorna um lista de strings com os nomes dos arqivos
 """
-def listDirFiles(dirPath: str) -> [str]: 
+def listDirFiles(dirPath: str) -> 'list[str]': 
   onlyfiles = [f for f in listdir(dirPath) if isfile(join(dirPath, f))]
   return onlyfiles;
 
